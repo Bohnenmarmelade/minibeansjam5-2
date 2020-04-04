8bbc14b1d5d7293a71af9910828689eb896c7a20
bttrs <luca@luca.wtf>
2020-04-04 20:21:46 +0200

    parallax background

---
+++ Assets/Scripts/background/BuildingMovement.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class BuildingMovement : MonoBehaviour {
+    public Camera camera;
+
+    [SerializeField]
+    [Range(1f, 5f)] private float movementMultiplier = 2f;
+
+
+    private void Update() {
+        Vector3 pos = transform.position;
+        pos.x = camera.transform.position.x / movementMultiplier;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/background/FogMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FogMovement : MonoBehaviour {
+    public Camera camera;
+
+    [SerializeField]
+    [Range(1f, 5f)] private float movementMultiplier = 3f;
+
+
+    private void Update() {
+        Vector3 pos = transform.position;
+        pos.x = camera.transform.position.x / movementMultiplier;
+        transform.position = pos;
+    }
+}
